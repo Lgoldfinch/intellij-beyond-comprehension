361eac13092e06d86ff844258a9a833d839c98eb
GitHub Action <action@github.com>
2023-04-16 20:15:57 +0000

    Template cleanup

--- src/main/kotlin/com/github/lgoldfinch/intellijbeyondcomprehension/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/lgoldfinch/intellijbeyondcomprehension/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.lgoldfinch.intellijbeyondcomprehension.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.lgoldfinch.intellijbeyondcomprehension.MyBundle
+import com.github.lgoldfinch.intellijbeyondcomprehension.services.MyProjectService
 import javax.swing.JButton
 
 
